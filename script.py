0b00d8c4ea151818a5a956739f302019ee1ba7cc
Marine Germain <marine.germain@etu.umontpellier.fr>
2024-10-10 13:07:20 +0200

    modification de script

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import biketrauma
-from .io.Load_db import Load_db
-from .vis.plot_location import plot_location
-from .preprocess.get_accident import get_accident
+from biketrauma.io.Load_db import Load_db
+from biketrauma.vis.plot_location import plot_location
+from biketrauma.preprocess.get_accident import get_accident
 
 df = biketrauma.Load_db().save_as_df()
 biketrauma.plot_location(biketrauma.get_accident(df))
