e264a024bdc46dd234f5587b97c22b9cbec5eabb
Marine Germain <marine.germain@etu.umontpellier.fr>
2024-10-10 13:05:13 +0200

    modif3

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import biketrauma
-from .io.Load_db import Load._db
+from .io.Load_db import Load_db
 from .vis.plot_location import plot_location
 from .preprocess.get_accident import get_accident
 
